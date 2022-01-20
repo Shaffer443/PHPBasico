52b358e3ed20fcdbed4254265879e686699cf45c
Shaffer443 <rafaelgouveiamelo@yahoo.com.br>
2022-01-20 12:56:24 -0300

    primeiros arquivos de POO e PHP

--- README.md
+++ README.md
@@ -1,14 +1,4 @@
 # PHP Básico - Curso em Vídeo
 
-Arquivos dos exercícios ou explicações sobre funções. modelos em PHP e HTML5, com um pouco de CSS. 
-
-Com foco nas atividades do Curso Básico, feito e concluído no Curso em Vídeo (@cursoemvideo), que é voltado para trabalhar itens e lógicas básicas em PHP.
-
-da Aula 04 a 18. Totalizando 40h, com certificado.
-
-[Certificado de Conclusão]https://drive.google.com/file/d/1QdbmezZ0bq-nT-rGq8riS884Bj83n4D3/view?usp=sharing
-
-OBS: Falta a aula 17, que deu erro na hora de subir todo o script da aula. 
-
-Iniciar PHP POO | Fase 2
-
+Arquivos, Exercícios e modelos em PHP e HTML5, com um pouco de CSS. 
+Com foco nas atividades do curso, qeu é voltado para trabalhar itens e lógicas básicas em PHP.
---
+++ netbeans/aula03/exercicio01.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+/**
+ * Description of exercicio01
+ *
+ * @author Rafael Gouveia
+ */
+class Computador{
+    //criar classes para um objeto do meu dia a dia. Escolhi o pc.
+    
+    var $tipo; //Pc ou notebook
+    var $sistema; // Windows ou Linus
+    var $status; //ligado ou desligado
+    
+    function uso(){
+        //Premissa de montar conjuntos de uso para o pc.
+        
+        if($this->status == true){
+            
+            echo "<p> Pc ou Notebook: <b>Ligados</b></p>";
+        
+            
+        } else{
+        
+            echo "<p> Pc ou Notebook: <b>Desligado</b></p>";
+        }
+        
+    }
+    
+    function modelo(){
+        
+        echo "Qual o Modelo: 1 - Pc | 2 -Notebook";
+        escolha = INPUT_GET[];
+        if($this->tipo == 1){
+            echo "Você está usando um PC";
+        }else{
+            echo "Você está usando um Notebook";
+        }
+    }
+    
+}
